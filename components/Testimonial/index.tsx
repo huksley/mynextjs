97e26205e2ef7ace1c2f8fbbdd2da8c4ce71c684
Ruslan Gainutdinov <ruslan@wizecore.com>
2026-04-09 18:52:49 +0300

    Update index.tsx

--- components/Testimonial/index.tsx
+++ components/Testimonial/index.tsx
@@ -19,7 +19,7 @@ const Testimonial = () => {
           {/* <!-- Section Title Start --> */}
           <div className="animate_top mx-auto text-center">
             <SectionHeader
-              headerInfo2={{
+              headerInfo={{
                 title: `TESTIMONIALS`,
                 subtitle: `Client’s Testimonials`,
                 description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor eros. Donec vitae tortor lacus. Phasellus aliquam ante in maximus.`,
